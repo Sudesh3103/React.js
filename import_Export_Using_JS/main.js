e203a7816c5c2964b63b11beef0c32cb933df380
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-01 17:50:40 +0530

    added import export code using  JS

---
+++ import_Export_Using_JS/main.js
@@ -0,0 +1,7 @@
+let a = 10;
+
+const user = "sudesh";
+
+export const arr = [1,2,3,4,5];//named export
+
+export default user ;//export default a;
---
+++ import_Export_Using_JS/script.js
@@ -0,0 +1,6 @@
+import a from './main.js'
+import {arr} from '/main.js'
+
+
+console.log(a);
+console.log(arr);
