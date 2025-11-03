dd66613c7f0a35463c34e33a1741132ce1040f74
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-03 23:07:29 +0530

    added react fragment use

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <>
-      <h1>Hello, React!</h1>
-    </>
+    <div>
+      <h1>Welcome to React</h1>
+      <h2>Hiii</h2>
+    </div>
+
+    // We can not write multiple elements inside one function it will throw error
+    // <h1>Hello</h1>
+
+    // to wrap the multiple Elements into a single function 
+    // without creating multiple divs we use react fragments
+    // i.e  <> </>
   )
 }
 
 export default App
+
