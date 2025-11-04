64a080f049eb2d3aa754d2b9141136e3d6a3c7e1
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-04 23:15:32 +0530

    updated some img code

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -3,8 +3,8 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user='Sudesh' age={25}/>
-      <Card user='Shubham' age={24}/>
+      <Card user='Sudesh' age={25} img='https://images.unsplash.com/photo-1761991062157-df67faab3a23?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600'/>
+      <Card user='Shubham' age={24} img='https://plus.unsplash.com/premium_photo-1757100707921-070878fe3796?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600'/>
     </div>
   )
 }
--- 03-props/src/components/Card.jsx
+++ 03-props/src/components/Card.jsx
@@ -7,8 +7,8 @@ const Card = (props) => {
   return (
     <div>
       <div className='card'>
-        <img src="https://images.unsplash.com/photo-1761939433931-4305ebba3ec9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=875" alt="" />
-        <h1>{props.user}</h1>
+        <img src={props.img} alt="" />
+        <h1>{props.user}, {props.age}</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, molestias.</p>
         <button className='card-button'>View Profile </button>
       </div>
