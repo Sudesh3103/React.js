75e06fecf4d4cc581b33c3c74e7a69983405d067
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-04 22:52:21 +0530

    Created a props  folder

---
+++ 03-props/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <img src="https://images.unsplash.com/photo-1761939433931-4305ebba3ec9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=875" alt="" />
+        <h1>Sudesh Kalokhe</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, molestias.</p>
+        <button className='card-button'>View Profile </button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
