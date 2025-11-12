8f80301b17b0f4ec482d96d459d7e8a00e5cae9f
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-12 23:29:01 +0530

    Added module.css for the css separation for each component

--- 05-css/src/App.jsx
+++ 05-css/src/App.jsx
@@ -1,5 +1,5 @@
-import Button from "./components/Button"
-import Header from "./components/Header"
+import Button from "./components/button/Button"
+import Header from "./components/header/Header"
 
 const App = () => {
   return (
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <h1>Logo</h1>
+        <button className={styles.btn}>Login</button>
+    </div>
+  
+    
+    
+  )
+}
+
+export default Header
