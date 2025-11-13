1b130b36df66d18bd6a9a9f65e10c97c79b736b5
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-13 23:22:49 +0530

    applied tailwind css

--- 06-tailwind/src/App.jsx
+++ 06-tailwind/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>This is Tailwind CSS </h1>
+    <div className=' h-screen w-full bg-gray-900' >
+      <h1 className='text-3xl text-white font-bold'>This is Tailwind CSS </h1>
     </div>
   )
 }
