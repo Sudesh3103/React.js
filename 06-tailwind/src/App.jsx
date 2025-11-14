96fe74cc3996a910199470b89fa7654851966475
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-14 22:13:52 +0530

    Changes in code

--- 06-tailwind/src/App.jsx
+++ 06-tailwind/src/App.jsx
@@ -4,6 +4,14 @@ const App = () => {
   return (
     <div className=' h-screen w-full bg-gray-900' >
       <h1 className='text-3xl text-white font-bold'>This is Tailwind CSS </h1>
+      <div className='flex justify-around text-white'>
+        <div>
+          <h2>Logo</h2>
+        </div>
+        <div>
+          <span>Home</span>
+        </div>
+      </div>
     </div>
   )
 }
