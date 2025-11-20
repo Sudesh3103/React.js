27e83ebf08a61a3c1827c72357a3fe453a83c942
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-20 10:23:43 +0530

    changes in code alignment

---
+++ .history/06-tailwind/src/App_20251120102154.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+     <div class="flex flex-row">
+       <div class="basis-64 bg-amber-300">01</div>
+        <div class="basis-64 bg-amber-500">02</div>
+  <div class="basis-128 bg-amber-700">03</div>
+  </div>
+
+  <button class="bg-blue-500 m-4">Button A</button>
+  <button class="bg-cyan-500 m-6">Button B</button>
+  <button class="bg-pink-500 m-8">Button C</button>
+    </>
+
+
+  
+  )
+}
+
+export default App
--- .history/06-tailwind/src/App_20251120102219.jsx
+++ .history/06-tailwind/src/App_20251120102219.jsx
@@ -4,9 +4,9 @@ const App = () => {
   return (
     <>
      <div class="flex flex-row">
-        <div class="basis-64 bg-amber-300">01</div>
-        <div class="basis-64 bg-amber-500">02</div>
-        <div class="basis-128 bg-amber-700">03</div>
+       <div class="basis-64 bg-amber-300">01</div>
+       <div class="basis-64 bg-amber-500">02</div>
+       <div class="basis-128 bg-amber-700">03</div>
      </div>
 
      <button class="bg-blue-500 m-4">Button A</button>
