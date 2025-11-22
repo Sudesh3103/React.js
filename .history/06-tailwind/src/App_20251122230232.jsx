67d615306a02479496e93b4fbbe64519cf1a3a75
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-22 23:02:52 +0530

    Code changes

---
+++ .history/06-tailwind/src/App_20251122230232.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+
+     <button class="bg-blue-500 m-4">Button A</button>
+     <button class="bg-cyan-500 m-6">Button B</button>
+     <button class="bg-pink-500 m-8">Button C</button>
+
+
+    </>
+
+
+  
+  )
+}
+
+export default App
