f059618c8b9613fd3fc60bb4c111ca1391de8371
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-23 19:31:50 +0530

    Created a project

---
+++ .history/07-ui-project/src/App_20251123192439.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to my React App</h1>
+      <p>This is a simple React app created using create-react-app.</p>
+    </div>
+  )
+}
+
+export default App
