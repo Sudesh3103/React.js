b46eee0f80adc833745d84627831c9e792dd7fe2
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-28 23:03:24 +0530

    Changes in code

--- 07-ui-project/src/components/section1/HeroText.jsx
+++ 07-ui-project/src/components/section1/HeroText.jsx
@@ -4,7 +4,7 @@ const HeroText = () => {
   return (
     <div className='p-5'>
         <h3 className='text-4xl mb-6 font-bold leading-[1.1]'>Prospective <br /> <span>customer</span> <br /> segmentation </h3>
-        <p className='text-sm font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi aliquam nulla quaerat quisquam architecto molestiae accusamus fuga eveniet ratione.</p>
+        <p className='text-sm  font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi aliquam nulla quaerat quisquam architecto molestiae accusamus fuga eveniet ratione.</p>
     </div>
   )
 }
--- 07-ui-project/src/components/section1/RightContent.jsx
+++ 07-ui-project/src/components/section1/RightContent.jsx
@@ -5,7 +5,7 @@ const RightContent = (props) => {
   return (
     <div className='h-full flex flex-nowrap overflow-x-auto gap-10 w-3/3 p-6'>
       {props.users.map(function(elem) {
-        return <RightCard img={elem.img} user={elem} />
+        return <RightCard img={elem.img} user={elem}  />
       })}
         
 
