03bc1953df3262251c68004ec77b2103612534d4
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-11-29 13:52:29 +0530

    updated code

--- 07-ui-project/src/components/section1/RightCard.jsx
+++ 07-ui-project/src/components/section1/RightCard.jsx
@@ -6,6 +6,7 @@ const RightCard = (props) => {
     <div className='h-full w-80 overflow-hidden relative rounded-4xl'>
       <img className='h-full w-full object-cover opacity-90 ' src={props.img} alt="" />
       <RightCardContent />
+      
     </div>
   )
 }
