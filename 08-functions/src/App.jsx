98615b69926c023efc4b5a5bad079ace73a07b27
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-12-02 17:50:25 +0530

    Added some function based code

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 const App = () => {
 
-  function btnclick(){
-    console.log("Button Clicked");
+  // function btnclick(){
+  //   console.log("Button Clicked");
+  // }
+
+  // function mouseEnter(){
+  //   console.log('Mouse Entered');
+  // }
+
+  function usertyping(elem){
+    console.log(elem.target.value);
   }
 
-  function mouseEnter(){
-    console.log('Mouse Entered');
-  }
   return (
     
-      <button onMouseEnter={mouseEnter} onDoubleClick={btnclick}>Click Here</button>
+      // <button onMouseEnter={mouseEnter} onDoubleClick={btnclick}>Click Here</button>
+      <input  onChange={usertyping}  type="text" placeholder='Enter your Name' />
     
   )
 }
