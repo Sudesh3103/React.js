65aff2ea93af074d7ad970569528ecc89ceaac8f
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-12-03 23:10:28 +0530

    Change in Code

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 
 const App = () => {
 
-  // function btnclick(){
-  //   console.log("Button Clicked");
-  // }
+  function btnclick(){
+    console.log("Button Clicked");
+  }
 
-  // function mouseEnter(){
-  //   console.log('Mouse Entered');
-  // }
+  function mouseEnter(){
+    console.log('Mouse Entered');
+  }
 
   function usertyping(elem){
     console.log(elem.target.value);
   }
 
   return (
+    <>
+     <button onMouseEnter={mouseEnter} onDoubleClick={btnclick}>Click Here</button>
+     <input  onChange={usertyping}  type="text" placeholder='Enter your Name' />
     
-      // <button onMouseEnter={mouseEnter} onDoubleClick={btnclick}>Click Here</button>
-      <input  onChange={usertyping}  type="text" placeholder='Enter your Name' />
-    
+    </>
+     
   )
 }
 
