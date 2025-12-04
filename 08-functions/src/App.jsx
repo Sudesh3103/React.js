4864a912dcc206430f424ff0e187e28af978b89e
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-12-04 14:48:55 +0530

    Added some code

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -14,10 +14,27 @@ const App = () => {
     console.log(elem.target.value);
   }
 
+  const pagesrolling = (elem) => {
+    if(elem>0){
+      console.log('Scrolling Up');
+    }
+    else{
+      console.log('Scrolling Down');
+    }
+  }
+
   return (
     <>
      <button onMouseEnter={mouseEnter} onDoubleClick={btnclick}>Click Here</button>
      <input  onChange={usertyping}  type="text" placeholder='Enter your Name' />
+
+     <div onWheel={(elem)=>{
+        pagesrolling(elem.deltaY);
+     }}>
+      <div className='page1'></div>
+      <div className='page2'></div>
+      <div className='page3'></div>
+     </div>
     
     </>
      
