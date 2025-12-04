abc13b132c5c1730b245aad07e6bd34ddfb23ef8
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-12-04 22:49:33 +0530

    Added counter using useState hook

--- 09-useState-hook/src/App.jsx
+++ 09-useState-hook/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <button onClick={() => setCount(count+1)}>+</button>
+      <input type="text" placeholder={count}  />
+      <button onClick={() => count>0 && setCount(count-1)}>-</button>
     </div>
   )
 }
