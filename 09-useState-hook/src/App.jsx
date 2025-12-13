117272f1d0895b2d4410e9f862a93f780e63b439
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-12-13 19:48:36 +0530

    Changes

--- 09-useState-hook/src/App.jsx
+++ 09-useState-hook/src/App.jsx
@@ -8,16 +8,12 @@ const App = () => {
 
   return (
     <div>
-
       <Timefetch />
-
       <hr /> 
-
       <h2>Counter using React Hooks - useState</h2>
       <button onClick={() => setCount(count+1)}>+</button>
       <input type="text" placeholder={count}  />
       <button onClick={() => count>0 && setCount(count-1)}>-</button>
-
     </div>
   )
 }
