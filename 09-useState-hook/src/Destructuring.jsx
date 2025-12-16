ebdf9b0e408e99807bbb8effb1cb29253792481f
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-12-16 23:02:33 +0530

    Code Added

--- 09-useState-hook/src/Destructuring.jsx
+++ 09-useState-hook/src/Destructuring.jsx
@@ -4,7 +4,9 @@ import { useState } from'react'
 const Destructuring = () => {
 
   const [num, setNum] = useState({user:"Sudesh", age:24});
+  const [number, setNumber] = useState([10,20,30,40]); 
 
+  //For object destructuring
   const btnclickeed = () =>{
     const newNum = {...num};
     newNum.user = "Shubham";
@@ -12,10 +14,20 @@ const Destructuring = () => {
     setNum(newNum);
   }
 
+  //For array destructuring
+  const handleClick = () => {
+    const newNumber = [...number];
+    newNumber.push(50);
+    setNumber(newNumber);
+  }
+
   return (
     <div>
       <h1>{num.user}, {num.age}</h1>
       <button onClick={btnclickeed}>Click</button>
+
+      <h2>{number}</h2>
+      <button onClick={handleClick}>Add 50</button>
     </div>
   )
 }
