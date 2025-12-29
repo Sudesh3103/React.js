adcb973e7d9b465313aaa95ac800571f827c36fa
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2025-12-29 23:01:01 +0530

    Code Updated

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div className='h-screen bg-black text-white'>
-
       <form className='flex justify-between flex-col gap-5 p-10 '>
         <input type="text" placeholder='Enter notes heading' className='w-1/2 px-5 py-2 border-2 rounded '/>
         <input type="text" placeholder='Write Details' className='w-1/2 h-32 px-5 py-2 border-2 rounded '/>
         <button className='bg-white w-full outline-none text-black px-5 py-2 rounded'>Add Notes</button>
       </form>
-      
     </div>
   )
 }
