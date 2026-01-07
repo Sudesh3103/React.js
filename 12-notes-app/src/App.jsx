366bb638e184c744a719d4b233e4fd827274a372
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2026-01-07 22:27:16 +0530

    Updated

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='h-screen bg-black text-white'>
+
       <form className='flex justify-between items-start  p-10 '>
         <div className='flex gap-4 w-1/2 items-start flex-col'>
            <input type="text" placeholder='Enter notes heading' className='w-full px-5 py-2 border-2 rounded '/>
@@ -11,6 +12,7 @@ const App = () => {
         </div>
         <img src="" alt="" />
       </form>
+      
     </div>
   )
 }
