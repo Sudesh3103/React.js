3a1cd98e96b94994e6e70c295973ff4bafc01f2d
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2026-01-07 22:53:59 +0530

    changes in code

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div className='h-screen bg-black text-white'>
 
-      <form className='flex justify-between items-start  p-10 '>
-        <div className='flex gap-4 w-1/2 items-start flex-col'>
-           <input type="text" placeholder='Enter notes heading' className='w-full px-5 py-2 border-2 rounded '/>
-           <input type="text" placeholder='Write Details' className='w-full h-32 px-5 py-2 border-2 rounded '/>
-           <button className='bg-white w-full text-black px-5 py-2 rounded'>Add Notes</button>
+  const submitHandler =(e) =>{
+    e.preventDefault();
+    console.log("form submitted");
+  }
+
+
+  return (
+    <div className="h-screen bg-black text-white">
+      <form onSubmit={submitHandler} className="flex justify-between items-start  p-10 ">
+        <div className="flex gap-4 w-1/2 items-start flex-col">
+          <input
+            type="text"
+            placeholder="Enter notes heading"
+            className="w-full px-5 py-2 border-2 rounded "
+          />
+          <textarea
+            type="text"
+            placeholder="Write Details"
+            className="w-full h-32 px-5 py-2 border-2 rounded "
+          />
+          <button className="bg-white w-full text-black px-5 py-2 rounded">
+            Add Notes
+          </button>
         </div>
         <img src="" alt="" />
       </form>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
