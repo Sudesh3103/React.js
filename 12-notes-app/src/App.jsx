93935e7c50defa9e0021a15b48e3a879612f5ac2
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2026-01-30 18:03:20 +0530

    Changes

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
           </button>
       </form>
 
-      <div className=" bg-gray-900 lg:w-1/2  p-10">
+      <div className=" bg-gray-900 border-l-2 lg:w-1/2  p-10">
         <h1 className="text-xl font-bold">Your Notes</h1>
         <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
           <div className="h-50 w-40 rounded-2xl bg-white"></div>
