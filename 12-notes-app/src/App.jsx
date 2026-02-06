dd7cfb5ad271b2a7165b9e237ac2e52410276542
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2026-02-06 23:52:11 +0530

    Code Updated

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -3,10 +3,18 @@ const App = () => {
   
   const [title, setTitle] = useState('');
   const [details, setDetails] = useState('');
+  const [task, setTask] = useState([]);
 
   const submitHandler =(e) =>{
     e.preventDefault();
-    console.log(title, details);
+
+    const copyTask = [...task];
+    copyTask.push({title:title,details:details});
+    setTask(copyTask);
+
+    // console.log(copyTask);
+    // console.log(title);
+    // console.log(details);
 
     setTitle('');
     setDetails('');
@@ -41,20 +49,23 @@ const App = () => {
             }}
           />
 
-          <button className="bg-white w-full text-black px-5 py-2 rounded">
+          <button className="bg-white active:scale-110 w-full text-black px-5 py-2 rounded">
             Add Notes
           </button>
       </form>
 
       <div className=" bg-gray-900 lg:border-l-2 lg:w-1/2  p-10">
         <h1 className="text-xl font-bold">Recent Notes</h1>
-        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
-          <div className="h-50 w-40 rounded-2xl bg-white"></div>
-          <div className="h-50 w-40 rounded-2xl bg-white"></div>
-          <div className="h-50 w-40 rounded-2xl bg-white"></div>
+        <div className="flex flex-wrap items-startgap-5 mt-5 h-full overflow-auto">
+          {task.map(function (elem, idx) {
+            return  <div key={idx} className="h-52 w-40 rounded-xl text-black p-4 bg-white">
+              <h3>{elem.title}</h3>
+              <p className="text-sm mt-2">{elem.details}</p>
+            </div>
+          })}
         </div>
       </div>
-      
+      ``
     </div>
   );
 };
